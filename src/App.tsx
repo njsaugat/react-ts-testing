5a893fdbf35ab145db4745f00649c785e5abb27f
njsaugat <njsaugat@gmail.com>
2023-12-22 11:38:52 +0545

    add the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./rtl/Counter";
 
 function App() {
   return (
     <div className="App">
+      <Counter />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
